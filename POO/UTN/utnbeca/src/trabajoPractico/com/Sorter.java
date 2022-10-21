d73eae1547acb48233b101182b5dada5c7bbc47f
alex08-vt <alex.velasquez08@outlook.com>
2022-10-21 00:22:40 -0300

    Archivos armado

--- POO/UTN/utnbeca/src/trabajoPractico/com/Sorter.java
+++ POO/UTN/utnbeca/src/trabajoPractico/com/Sorter.java
@@ -1,2 +1,10 @@
-package trabajoPractico.com;public interface Sorter {
+package trabajoPractico.com;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public interface Sorter extends Comparator<Sorter>{
+    public <T> void sort(T arr[],Comparator<T> c);
 }
+
+
